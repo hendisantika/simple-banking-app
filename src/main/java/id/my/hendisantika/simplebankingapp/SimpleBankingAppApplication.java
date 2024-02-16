240dd8cabe88291a4e3957d4aa5cff3d08b6f035
hendisantika <hendisantika@yahoo.co.id>
2024-02-17 06:52:14 +0700

    Create starter project ✏️📗 :octocat:⬆

---
+++ src/main/java/id/my/hendisantika/simplebankingapp/SimpleBankingAppApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.simplebankingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBankingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/simplebankingapp/SimpleBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.simplebankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
