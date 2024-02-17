46c93c10ebca50eb4fa5ec405297e75f95df03b6
hendisantika <hendisantika@yahoo.co.id>
2024-02-18 06:11:32 +0700

    Add UtilsTests 🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/simplebankingapp/util/UtilsTests.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.simplebankingapp.util;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : simple-banking-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/18/24
+ * Time: 06:11
+ * To change this template use File | Settings | File Templates.
+ */
+@ExtendWith(SpringExtension.class)
+public class UtilsTests {
+}
