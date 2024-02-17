b2dc52008ffbfab29c7696133b119892448cb3c7
hendisantika <hendisantika@yahoo.co.id>
2024-02-18 06:13:28 +0700

    Add testPublishMessageSuccessfully in MessagePublisherTests 🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/simplebankingapp/config/MessagePublisherTests.java
+++ src/test/java/id/my/hendisantika/simplebankingapp/config/MessagePublisherTests.java
@@ -2,7 +2,9 @@ package id.my.hendisantika.simplebankingapp.config;
 
 import id.my.hendisantika.simplebankingapp.model.Account;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -31,5 +33,11 @@ public class MessagePublisherTests {
     void setUp() {
         messagePublisher = new MessagePublisher(rabbitTemplate, queue);
         messagePublisher.publishAccountDetails(new Account());
+    }
+
+    @Test
+    void testPublishMessageSuccessfully() {
+        Mockito.verify(rabbitTemplate, Mockito.times(1))
+                .convertAndSend(Mockito.any(), Mockito.any(Account.class));
     }
 }
