c0353c42e3f570c9959a70c4509f498c85fd4dd7
hendisantika <hendisantika@yahoo.co.id>
2024-02-17 07:03:45 +0700

    Add GlobalExceptionHandler 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/simplebankingapp/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.simplebankingapp.exception.handler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : simple-banking-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/17/24
+ * Time: 07:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class GlobalExceptionHandler {
+}
