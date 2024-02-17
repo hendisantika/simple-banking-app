aa58246f0e4d4701dd72b43a2acc77f58faa59e3
hendisantika <hendisantika@yahoo.co.id>
2024-02-18 06:18:33 +0700

    Add BeforeEach in TransactionServiceTests 🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/simplebankingapp/service/TransactionServiceTests.java
+++ src/test/java/id/my/hendisantika/simplebankingapp/service/TransactionServiceTests.java
@@ -1,14 +1,20 @@
 package id.my.hendisantika.simplebankingapp.service;
 
+import id.my.hendisantika.simplebankingapp.UnitTestHelper;
 import id.my.hendisantika.simplebankingapp.config.MessagePublisher;
+import id.my.hendisantika.simplebankingapp.exception.ApiException;
 import id.my.hendisantika.simplebankingapp.mapper.AccountMapper;
 import id.my.hendisantika.simplebankingapp.mapper.TransactionMapper;
 import id.my.hendisantika.simplebankingapp.model.Account;
 import id.my.hendisantika.simplebankingapp.model.Balance;
 import id.my.hendisantika.simplebankingapp.model.Transaction;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,4 +42,18 @@ public class TransactionServiceTests {
     private Account account;
     private Balance balance;
     private Transaction transaction;
+
+    @BeforeEach
+    void setUp() {
+        transactionService = new TransactionService(publisher, balanceService, accountMapper, transactionMapper);
+        account = UnitTestHelper.getAccount();
+        balance = UnitTestHelper.getBalance();
+        transaction = UnitTestHelper.getTransaction();
+        try {
+            Mockito.when(balanceService.updateAmountOfBalance(Mockito.any())).thenReturn(balance);
+            Mockito.when(accountMapper.findById(Mockito.anyLong())).thenReturn(Optional.of(account));
+        } catch (ApiException exception) {
+            exception.printStackTrace();
+        }
+    }
 }
