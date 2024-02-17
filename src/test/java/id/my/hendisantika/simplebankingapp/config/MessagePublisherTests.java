a709bfa6cc5d00443aae7132d33fc3a5e83da812
hendisantika <hendisantika@yahoo.co.id>
2024-02-18 06:12:51 +0700

    Add MessagePublisherTests 🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/simplebankingapp/config/MessagePublisherTests.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.simplebankingapp.config;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : simple-banking-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/18/24
+ * Time: 06:12
+ * To change this template use File | Settings | File Templates.
+ */
+@ExtendWith(SpringExtension.class)
+public class MessagePublisherTests {
+    private final String queueName = "accountQueue";
+    @MockBean
+    private RabbitTemplate rabbitTemplate;
+    @MockBean
+    private Queue queue;
+    private MessagePublisher messagePublisher;
+}
