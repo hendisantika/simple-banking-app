fca67f78601817eddc0c2ff04adb7ea708b83ce9
hendisantika <hendisantika@yahoo.co.id>
2024-02-17 07:17:36 +0700

    Add createTransaction in TransactionController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/simplebankingapp/controller/TransactionController.java
+++ src/main/java/id/my/hendisantika/simplebankingapp/controller/TransactionController.java
@@ -1,8 +1,17 @@
 package id.my.hendisantika.simplebankingapp.controller;
 
+import id.my.hendisantika.simplebankingapp.dto.TransactionResponse;
+import id.my.hendisantika.simplebankingapp.exception.ApiException;
+import id.my.hendisantika.simplebankingapp.form.TransactionForm;
 import id.my.hendisantika.simplebankingapp.service.TransactionService;
+import id.my.hendisantika.simplebankingapp.util.Utils;
 import id.my.hendisantika.simplebankingapp.validator.TransactionFormValidator;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,4 +29,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransactionController {
     private final TransactionFormValidator validator;
     private final TransactionService transactionService;
+
+    @PostMapping("/api/transaction/create")
+    public TransactionResponse createTransaction(@Valid @RequestBody TransactionForm form,
+                                                 BindingResult bindingResult)
+            throws ApiException {
+        System.out.println("TRNX requested");
+        validator.validate(form, bindingResult);
+        if (bindingResult.hasErrors()) {
+            throw new ApiException(HttpStatus.NOT_ACCEPTABLE,
+                    Utils.getErrorMessage(bindingResult));
+        }
+
+        return transactionService.createTransaction(form.toModel());
+    }
 }
