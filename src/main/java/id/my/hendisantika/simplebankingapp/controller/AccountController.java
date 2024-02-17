cef57d8f3a8494d5da5ce294b16dee823a3c53ad
hendisantika <hendisantika@yahoo.co.id>
2024-02-17 07:16:10 +0700

    Add createAccount in AccountController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/simplebankingapp/controller/AccountController.java
+++ src/main/java/id/my/hendisantika/simplebankingapp/controller/AccountController.java
@@ -1,8 +1,17 @@
 package id.my.hendisantika.simplebankingapp.controller;
 
+import id.my.hendisantika.simplebankingapp.dto.AccountResponse;
+import id.my.hendisantika.simplebankingapp.exception.ApiException;
+import id.my.hendisantika.simplebankingapp.form.AccountForm;
 import id.my.hendisantika.simplebankingapp.service.AccountService;
+import id.my.hendisantika.simplebankingapp.util.Utils;
 import id.my.hendisantika.simplebankingapp.validator.AccountFormValidator;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,4 +29,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
     private final AccountFormValidator validator;
     private final AccountService accountService;
+
+    @PostMapping("/api/account/create")
+    public AccountResponse createAccount(@Valid @RequestBody AccountForm form,
+                                         BindingResult bindingResult)
+            throws ApiException {
+
+        validator.validate(form, bindingResult);
+        if (bindingResult.hasErrors()) {
+            throw new ApiException(HttpStatus.NOT_ACCEPTABLE,
+                    Utils.getErrorMessage(bindingResult));
+        }
+
+        return accountService.createAccount(form.toModel());
+    }
 }
