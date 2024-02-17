ec71028658e0b6f2b2384d3cafe179634ce45de6
hendisantika <hendisantika@yahoo.co.id>
2024-02-17 07:10:04 +0700

    Add @EnableTransactionManagement @EnableRabbit 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/simplebankingapp/SimpleBankingAppApplication.java
+++ src/main/java/id/my/hendisantika/simplebankingapp/SimpleBankingAppApplication.java
@@ -1,9 +1,13 @@
 package id.my.hendisantika.simplebankingapp;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableRabbit
 public class SimpleBankingAppApplication {
 
     public static void main(String[] args) {
