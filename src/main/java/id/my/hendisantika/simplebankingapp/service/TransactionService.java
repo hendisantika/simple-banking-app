b3ddd40fdb7e0441c0193d029fe2af2f7bfd3937
hendisantika <hendisantika@yahoo.co.id>
2024-02-17 07:14:57 +0700

    Add createTransaction in TransactionService 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/simplebankingapp/service/TransactionService.java
+++ src/main/java/id/my/hendisantika/simplebankingapp/service/TransactionService.java
@@ -1,9 +1,15 @@
 package id.my.hendisantika.simplebankingapp.service;
 
 import id.my.hendisantika.simplebankingapp.config.MessagePublisher;
+import id.my.hendisantika.simplebankingapp.dto.TransactionResponse;
+import id.my.hendisantika.simplebankingapp.exception.ApiException;
 import id.my.hendisantika.simplebankingapp.mapper.AccountMapper;
 import id.my.hendisantika.simplebankingapp.mapper.TransactionMapper;
+import id.my.hendisantika.simplebankingapp.model.Account;
+import id.my.hendisantika.simplebankingapp.model.Balance;
+import id.my.hendisantika.simplebankingapp.model.Transaction;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,4 +31,23 @@ public class TransactionService {
     private final BalanceService balanceService;
     private final AccountMapper accountMapper;
     private final TransactionMapper transactionMapper;
+
+    @Transactional
+    public TransactionResponse createTransaction(Transaction transaction) throws ApiException {
+        Balance balance = balanceService.updateAmountOfBalance(transaction);
+
+        try {
+            transactionMapper.insertTransaction(transaction);
+        } catch (Exception exception) {
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    exception.getMessage());
+        }
+
+        Account account = accountMapper.findById(transaction.getAccountId())
+                .orElseThrow(() -> new ApiException(HttpStatus.NOT_FOUND, "Account missing"));
+
+        publisher.publishAccountDetails(account);
+
+        return getTransactionResponse(transaction, balance.getAmount());
+    }
 }
